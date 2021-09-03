b4e9cfaef23d70c4f87c9af866f26f50d37ed07c
Melanie Wong <melwong98@g.ucla.edu>
2021-09-03 15:54:04 -0700

    Complete qualified assignment, ...

---
+++ Project_Local_Library_1/public/src/accounts.js
@@ -0,0 +1,19 @@
+function findAccountById(accounts, id) {
+  for (const id in accounts){
+    console.log(accounts);
+  }
+  return 
+}
+
+function sortAccountsByLastName(accounts) {}
+
+function getTotalNumberOfBorrows(account, books) {}
+
+function getBooksPossessedByAccount(account, books, authors) {}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
